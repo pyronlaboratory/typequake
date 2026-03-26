a3ce90bd48a9bfc126e480247167ba6f2a676b02
pyronlaboratory <ronnie94official@gmail.com>
2026-03-26 14:23:01 +0530

    Configured vitest

--- index.ts
+++ index.ts
@@ -1,1 +1,6 @@
-console.log("Hello via Bun!");
+export function getChangeType(oldType: string | null, newType: string | null) {
+  if (!oldType && newType) return "ADDITIVE";
+  if (oldType && !newType) return "REMOVED";
+  if (oldType !== newType) return "BREAKING";
+  return "UNCHANGED";
+}
